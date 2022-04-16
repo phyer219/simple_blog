82d4a098fefa9f1c7e4b9d815ca42e90b4cfae7d
zqw_archnuc <phyer219@gmail.com>
2022-04-16 21:10:48 +0800

    optimize s.py

--- s.py
+++ s.py
@@ -4,6 +4,8 @@ import subprocess
 import re
 import os
 import shutil
+import sys
+import getopt
 
 posts_source_path = './source/posts/'
 templates_path = './source/templates/'
@@ -66,42 +68,75 @@ class Post:
                             posts_public_path + self.name)
 
 
-def gen_posts():
-    if os.path.exists(posts_public_path):
-        shutil.rmtree(posts_public_path)
-    os.mkdir(posts_public_path)  # todo
-    if not os.path.exists(temp_path):
-        os.mkdir(temp_path)
-    posts = []
-    for p in os.listdir(posts_source_path):
-        if re.search(r'.md$', p) or re.search(r'.org$', p):
-            posts.append(Post(posts_source_path + p))
+def gen_single_post(name_ext):
+    """generate one post"""
+    post = Post(posts_source_path + name_ext)
+    print('--- Generating post ---', post.name)
+    post.gen_post()
 
-    posts.sort(key=lambda p:(p.year, p.month, p.day), reverse=True)
+
+def gen_index():
+    """generate index"""
+    posts = []
+    for p in os.listdir(posts_public_path):
+        if re.search(r'.html$', p):
+            posts.append(Post(posts_source_path + p))
+    posts.sort(key=lambda p: (p.year, p.month, p.day), reverse=True)
 
     with open('index.html', 'w') as t:
         for line in open(templates_path+'index_temp.html'):
             if re.search(r'{path}', line):
                 for post in posts:
                     print('--- Generating post ---', post.name)
-                    post.gen_post()
-                    new_line = line.replace(r'{path}', posts_public_path+post.name+'.html')  # todo
-                    new_line = new_line.replace(r'{title}', post.name + f'--{post.year:n}-{post.month:n}-{post.day:n}')
+                    new_line = line.replace(r'{path}',
+                                            posts_public_path+post.name+'.html')  # todo
+                    new_line = new_line.replace(r'{title}',
+                                                post.name + f'--{post.year:n}-{post.month:n}-{post.day:n}')
                     t.writelines(new_line)
             else:
                 t.writelines(line)
+
+
+def gen_all():
+    """generate all posts, and index"""
+    if os.path.exists(posts_public_path):
+        shutil.rmtree(posts_public_path)
+    os.mkdir(posts_public_path)  # todo
+    if not os.path.exists(temp_path):
+        os.mkdir(temp_path)
+    posts = []
+    for name_ext in os.listdir(posts_source_path):
+        if re.search(r'.md$', name_ext) or re.search(r'.org$', name_ext):
+            post = Post(posts_source_path + name_ext)
+            posts.append(post)
+            gen_single_post(name_ext)
+
+    gen_index()
     shutil.rmtree(temp_path)
+
 
 def s():
     h = SimpleHTTPRequestHandler
     s = HTTPServer(('', 8000), h)
-    print('sever!')
+    print('serving localhost:8000')
     s.serve_forever()
 
 
 # g_md('./md.md')
 # g_org('./lt.org')
-gen_posts()
-# gen_index()
-s()
+
+opts, args = getopt.getopt(sys.argv[1:], 'asip:')
+for opt, v in opts:
+    if opt == "-a":
+        # generate all posts and index
+        gen_all()
+    elif opt == "-s":
+        # start server
+        s()
+    elif opt == "-i":
+        # generate index
+        gen_index()
+    elif opt == "-p":
+        # generate one post, given filename.extension
+        gen_single_post(v)
 
